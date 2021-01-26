e23e35b3d1a914b30d5017dbe660b49495963771
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2021-01-26 18:41:58 +0200

    Add icon to textfield

--- Itinerary App/Views/AddTripViewController/AddTripViewController.swift
+++ Itinerary App/Views/AddTripViewController/AddTripViewController.swift
@@ -26,6 +26,27 @@ class AddTripViewController: UIViewController {
     }
     
     @IBAction func save(_ sender: UIButton) {
+        tripTextField.rightViewMode = .never
+        
+        guard tripTextField.text != "", let newTripName = tripTextField.text else {
+            //let image = UIImage(systemName: "envelope")
+            // https://medium.com/nyc-design/swift-4-add-icon-to-uitextfield-48f5ebf60aa1
+            let image = UIImage(named: "newWarn") 
+            
+            let iconView = UIImageView(frame: CGRect(x: 5, y: 5, width: 20, height: 20))
+            iconView.image = image
+            
+            let iconContainerView: UIView = UIView(frame: CGRect(x: 20, y: 0, width: 30, height: 30))
+               iconContainerView.addSubview(iconView)
+            tripTextField.rightView = iconContainerView
+            tripTextField.rightViewMode = .always
+            tripTextField.tintColor = .red // color of image inside
+            
+            
+            return
+        }
+        
+        TripFunctions.createTrip(tripModel: TripModel(title: newTripName))
         if let doneSaving = doneSaving {
             doneSaving()
         }
